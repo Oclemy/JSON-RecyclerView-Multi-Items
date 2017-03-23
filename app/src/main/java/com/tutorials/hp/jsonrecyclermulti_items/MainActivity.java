f34a734bbbc6fdbffde0c805b790eeac15f09d2f
Oclemy <oclemmi@gmail.com>
2017-03-22 17:41:11 -0700

    Second Commit

--- app/src/main/java/com/tutorials/hp/jsonrecyclermulti_items/MainActivity.java
+++ app/src/main/java/com/tutorials/hp/jsonrecyclermulti_items/MainActivity.java
@@ -2,14 +2,11 @@ package com.tutorials.hp.jsonrecyclermulti_items;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.tutorials.hp.jsonrecyclermulti_items.m_JSON.JSONDownloader;
 
